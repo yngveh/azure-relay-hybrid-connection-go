d88ea0d2550e2ed17b809ff080251ca5c9504944
Yngve Hansen <yngve.hansen2@avinor.no>
2020-07-01 20:16:41 +0200

    Rewrite client

--- client.go
+++ client.go
@@ -1,11 +1,9 @@
 package arhc
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
-	"time"
 
 	"github.com/Azure/azure-amqp-common-go/v3/auth"
 	"github.com/Azure/azure-amqp-common-go/v3/sas"
@@ -49,54 +47,15 @@ func NewClient(c *Config) (*Client, error) {
 	}, nil
 }
 
-func (c *Client) Get(uri string) ([]byte, error) {
+func (c *Client) NewRequest(method, url string, body io.Reader) (req *http.Request, err error) {
 
-	r, statusCode, err := c.httpRequest(c.url+uri, "GET", nil)
+	wsUrl := c.url + url
+	req, err = http.NewRequest(method, wsUrl, body)
 	if err != nil {
-		return nil, err
-	}
-
-	debug("StatusCode: %d ", *statusCode)
-
-	if *statusCode > 299 {
-		return nil, fmt.Errorf("statusCode: %d", *statusCode)
-	}
-
-	//var response *Response
-	//if err := json.Unmarshal(r, response); err != nil {
-	//	return nil, err
-	//}
-
-	debug("Response: %s", string(r))
-
-	return r, nil
-}
-
-func (c *Client) httpRequest(uri, method string, payload []byte, ) ([]byte, *int, error) {
-
-	httpClient := &http.Client{
-		Timeout: time.Second * 10,
-	}
-
-	req, err := http.NewRequest(method, uri, bytes.NewBuffer(payload))
-	if err != nil {
-		return nil, nil, err
+		return
 	}
 
 	req.Header.Add(authHeaderName, c.token.Token)
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return nil, nil, err
-	}
 
-	defer func() {
-		_ = resp.Body.Close()
-	}()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if body != nil {
-		debug("Body: %s", string(body))
-	}
-
-	return body, &resp.StatusCode, err
+	return
 }
--- examples/client/main.go
+++ examples/client/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	arhc "github.com/yngveh/azure-relay-hybrid-connection-go"
 )
 
 func main() {
 
-	log.Print("START")
-	defer log.Print("END")
-
+	// Read config from environment variables
 	c := &arhc.Config{
 		Namespace:      os.Getenv("HC_NAMESPACE"),
 		ConnectionName: os.Getenv("HC_CONNECTION_NAME"),
@@ -19,15 +20,34 @@ func main() {
 		Key:            os.Getenv("HC_KEY"),
 	}
 
+	// Create a azure relay hybrid connection client
 	client, err := arhc.NewClient(c)
 	if err != nil {
 		panic(err)
 	}
 
-	r, err := client.Get("/test")
+	// Use client.NewRequest instead of http.NewRequest
+	req, err := client.NewRequest("GET", "/test", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	log.Print("RESPONSE ==> ", string(r))
+	// Rest is treated as normal http calls
+	httpClient := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Print("RESPONSE ==> ", string(body))
 }
